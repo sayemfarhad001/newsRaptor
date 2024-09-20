9bc7c4f3872460b4053c244e86f9369b2106e45d
Sayem Farhad <sayemfarhad001@gmail.com>
2024-09-20 13:30:51 -0400

    pagination

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -13,11 +13,11 @@ export class NewsItem extends Component {
         return (
         <div className="my-3">
             <div className="card" style={{width: "18rem"}}>
-                <img src={imageUrl} className="card-img-top" alt="..."/>
+                <img src={imageUrl ? imageUrl:"https://compote.slate.com/images/22ce4663-4205-4345-8489-bc914da1f272.jpeg?crop=1560%2C1040%2Cx0%2Cy0&width=1280"} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read more</a>
                     </div>
                 </div>
         </div>
