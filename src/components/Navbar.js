d7f4272b941398c7c823b33d2c656b108f467ea8
Sayem Farhad <sayemfarhad001@gmail.com>
2024-09-20 11:47:34 -0400

    project sample data from newsApi

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
+
+export class Navbar extends Component {
+//   static propTypes = {
+
+//   }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">NewsRaptor</a>
+                
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span   span="true" className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className='d-flex'>
+                    <img className="border border-dark rounded-circle" src="/android-chrome-512x512.png" alt="NewsRaptor" width="50" height="50"/>
+                    <h1><strong>NewsRaptor</strong></h1>
+                </div>
+            </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navbar
