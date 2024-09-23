6ab9ae58e4c58b6c3b9618cc0e56fecfafa3f516
Sayem Farhad <sayemfarhad001@gmail.com>
2024-09-23 13:48:34 -0400

    remove buttons next and previous + add infinite scroll

--- src/App.js
+++ src/App.js
@@ -26,20 +26,21 @@ export default class App extends Component {
     }
   }
 
+  pageSize = 5;
+
   render() {
     return (
       <>
         <Router>
           <Navbar mode={this.state.mode} toggleMode={this.toggleMode}/>
           <Routes>
-            <Route exact path="/" element={<News key="general" pageSize={18} apiKey={apiKey} category="general" mode={this.state.mode}/>}/>
-            <Route path="/business" element={<News key="business" pageSize={18} apiKey={apiKey} category="business" mode={this.state.mode}/>}/>
-            <Route path="/entertainment" element={<News key="entertainment" pageSize={18} apiKey={apiKey} category="entertainment" mode={this.state.mode}/>}/>
-            <Route path="/general" element={<News pageSize={18} apiKey={apiKey} category="general"/>} mode={this.state.mode}/>
-            <Route path="/health" element={<News key="health" pageSize={18} apiKey={apiKey} category="health" mode={this.state.mode}/>}/>
-            <Route path="/science" element={<News key="science" pageSize={18} apiKey={apiKey} category="science" mode={this.state.mode}/>}/>
-            <Route path="/sports" element={<News key="sports" pageSize={18} apiKey={apiKey} category="sports" mode={this.state.mode}/>}/>
-            <Route path="/technology" element={<News key="technology" pageSize={18} apiKey={apiKey} category="technology" mode={this.state.mode}/>}/>
+            <Route exact path="/" element={<News key="general" pageSize={this.pageSize} apiKey={apiKey} category="general" mode={this.state.mode}/>}/>
+            <Route path="/business" element={<News key="business" pageSize={this.pageSize} apiKey={apiKey} category="business" mode={this.state.mode}/>}/>
+            <Route path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} apiKey={apiKey} category="entertainment" mode={this.state.mode}/>}/>
+            <Route path="/health" element={<News key="health" pageSize={this.pageSize} apiKey={apiKey} category="health" mode={this.state.mode}/>}/>
+            <Route path="/science" element={<News key="science" pageSize={this.pageSize} apiKey={apiKey} category="science" mode={this.state.mode}/>}/>
+            <Route path="/sports" element={<News key="sports" pageSize={this.pageSize} apiKey={apiKey} category="sports" mode={this.state.mode}/>}/>
+            <Route path="/technology" element={<News key="technology" pageSize={this.pageSize} apiKey={apiKey} category="technology" mode={this.state.mode}/>}/>
             <Route path="/about" element={<About mode={this.state.mode}/>}/> 
           </Routes> 
         </Router>
